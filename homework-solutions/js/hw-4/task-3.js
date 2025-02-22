477375343ec524c228a506358d80f7ab3625573a
Tikimilk <pervozvanskaya123@gmail.com>
2025-02-22 15:38:31 +0300

    HW_4

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,4 +6,19 @@
   преобразовываясь в number
 
 */
-
+let minAge = 18;
+let maxAge = 60;
+let age = "12"; 
+if (age && isNaN(age)){
+ console.log ("Incorrect data type")}
+ age = Number(age);
+if (age < minAge) {
+console.log("You don't have access cause your age is " + "age" + " It's less then")
+} else if (age >= minAge && age <= maxAge) {
+ console.log ("Welcome  !")
+}else if (age > maxAge) {
+ console.log("Keep calm and look Culture channel")
+}
+else {
+ console.log("Technical work")
+};
