856f45ea0b8b35429385109333ddaeea26cb9f46
Tikimilk <pervozvanskaya123@gmail.com>
2025-03-15 19:21:28 +0300

    HW_8

--- homework-solutions/js/hw-8/task-2.mjs
+++ homework-solutions/js/hw-8/task-2.mjs
@@ -18,8 +18,24 @@ const words = [
   'queue',
 ];
 
-function sortedByVowels(wordsArr) {
-  // Ваш код
+function sortedByVowels(wordsArr)
+{
+  return wordsArr.sort((a, b) => {
+    let vowelsA = 0;
+    let vowelsB = 0;
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] === 'a' || a[i] === 'e' || a[i] === 'i' || a[i] === 'o' || a[i] === 'u') {
+        vowelsA++;
+      }
+    }
+    for (let i = 0; i < b.length; i++) {
+      if (b[i] === 'a' || b[i] === 'e' || b[i] === 'i' || b[i] === 'o' || b[i] === 'u') {
+        vowelsB++;
+      }
+    }
+    return vowelsA - vowelsB;
+  });
 }
+console.log(sortedByVowels(words));
 
 export { sortedByVowels };
